aa8b39c808f954ee14f92b899557b3e6d126dd61
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-02-28 13:43:05 +0530

    blog route added in the server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const blogRoutes = require("./routes/blog.js");
 
 const app = express();
 
+app.use("/api", blogRoutes);
+
 app.listen(process.env.PORT, (req, res) => {
-    console.log("listening to server")
+  console.log("listening to server");
 });
