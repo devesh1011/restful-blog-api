c56b30aeb38ab5535a495846543bb378cbadb356
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2024-02-28 13:42:44 +0530

    new errorHandler middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,8 @@
+// Error Handler
+const handleError = (res, statusCode, message) => {
+  res.status(statusCode).json({ success: false, error: message });
+};
+
+module.exports = {
+  handleError,
+};
